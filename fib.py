a131ab3b5057cef6ec99e6778195b2929e38f455
Andrew Howard <andrew.howard@rackspace.com>
2016-06-20 18:21:45 -0500

    Actually write fib.py function

--- fib.py
+++ fib.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
 
+def fib(x):
+  previous = 0
+  current = 1
+  tempSum = 0 #(ie: next)
+  counter = 0
+  while (counter < x):
+    print current
+    tempSum = previous + current
+    previous = current
+    current = tempSum
+    counter += 1
+  #end while
+#end fib()
 
+fib(10)
