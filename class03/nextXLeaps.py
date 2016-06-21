f70fa37a7e359af2780b9044c25d06b7f797199d
Andrew Howard <andrew.howard@rackspace.com>
2016-06-20 20:05:21 -0500

    Add homework notes

---
+++ class03/nextXLeaps.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+def isLeap(year):
+  return True|False
+#end isLeap()
+
+def xLeapsAfter(numLeaps, year):
+#end xLeapsAfter()
+
+def nextXLeaps(numLeaps):
+#end nextXLeaps()
+
+def next20Leaps():
+  nextXLeaps(20)
+#end next20Leaps()
+
